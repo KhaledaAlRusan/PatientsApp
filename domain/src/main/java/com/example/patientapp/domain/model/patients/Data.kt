c29df69362576f341836dae7a8372a2b66a51dcd
KhaledaAlRusan <khaled66658032@gmail.com>
2023-04-07 17:43:38 -0400

    Feature Add Patient: - Add patient info page

--- domain/src/main/java/com/example/patientapp/domain/model/patients/Data.kt
+++ domain/src/main/java/com/example/patientapp/domain/model/patients/Data.kt
@@ -35,3 +35,8 @@ data class Data(
     //Local var
     var selected:Boolean = false
 )
+{
+    fun getPatientInfo():String{
+        return "lives in $address \n Email: Born on $birthdate"
+    }
+}
